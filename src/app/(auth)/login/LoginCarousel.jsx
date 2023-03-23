631c065ac60ca791378728115e367a2ddeaac271
aldomaz <aldomazrocket@gmail.com>
2023-03-23 12:36:12 -0500

    fix 1

--- src/app/(auth)/login/LoginCarousel.jsx
+++ src/app/(auth)/login/LoginCarousel.jsx
@@ -1,47 +1,48 @@
-"use client"
+'use client'
 
-import React, { useEffect, useState } from 'react';
-import { Box, Typography } from '@mui/material';
+import React, {useEffect, useState} from 'react';
+import {Box} from '@mui/material';
 import Carousel from 'react-material-ui-carousel'
 
-export default function LoginCarousel({data}) {
+export default function LoginCarousel({ data }){
+  const [items, setItems] = useState([]);
 
-    const [items, setItems] = useState([]);
+  useEffect(() => {
+    setItems(data);
+  }, [data]);
 
-    useEffect(() => {
-        setItems(data);
-    }, [data])
+  console.log(items);
 
-    console.log(items);
-
-    return (
-        <Carousel 
-            navButtonsAlwaysInvisible
-            indicators={false}
-            nav
-            sx={{
-                display: 'flex',
-                alignItems: 'center',
-                padding: '16px',
-                justifyContent: 'center',
-                textAlign: 'center',
-                width: '100%',
-                height: '100%',
-            }}
+  return (
+    <Carousel
+      navButtonsAlwaysInvisible
+      indicators={false}
+      nav
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        padding: "16px",
+        justifyContent: "center",
+        textAlign: "center",
+        width: "100%",
+        height: "100%",
+      }}
+    >
+      {items?.map((item) => (
+        <Box
+          key={item.id}
+          sx={{
+            width: "100%",
+            height: "100%",
+          }}
         >
-            {
-                items?.map( (item) => (
-                    <Box 
-                        key={item.id}
-                        sx={{
-                            width: '100%',
-                            height: '100%'
-                        }}
-                    >
-                        <img className="login_image" width="100%" src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${item.attributes.url}`}/>
-                    </Box>
-                ))
-            }
-        </Carousel>
-    )
+          <img
+            className="login_image"
+            width="100%"
+            src={`${process.env.NEXT_PUBLIC_STRAPI_URL}${item.attributes.url}`}
+          />
+        </Box>
+      ))}
+    </Carousel>
+  );
 }
